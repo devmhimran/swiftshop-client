9589615528d8ab6b19671ba057eff558cd0ae461
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-04-15 01:46:25 +0600

    Added Login With Firebase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -7,8 +7,26 @@ import {
     Typography,
 } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../Components/firebase.init';
 
 const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+      let errorMessage
+
+    const handleLogin = (e) =>{
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        signInWithEmailAndPassword(email, password)
+    }
+    console.log(user)
     return (
         <div className='container max-w-screen-xl lg:mx-auto lg:px-0 px-3 pb-20'>
             <Card color="transparent" shadow={false} className='w-[40%] mx-auto'>
@@ -18,12 +36,13 @@ const Login = () => {
                 <Typography color="gray" className="mt-1 font-normal">
                     Enter your details to Login.
                 </Typography>
-                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                <form onSubmit={handleLogin} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                     <div className="mb-4 flex flex-col gap-6">
-                        <Input size="lg" label="Email" />
-                        <Input type="password" size="lg" label="Password" />
+                        <Input size="lg" label="Email" type='email' name='email' />
+                        <Input type="password" size="lg" label="Password" name='password' />
+                        <p className='text-sm text-red-600'>{error ? error.message : ''}</p>
                     </div>
-                    <Button className="mt-6" fullWidth>
+                    <Button type='submit' className="mt-6" fullWidth>
                         Login
                     </Button>
                     <Typography color="gray" className="mt-4 text-center font-normal">
