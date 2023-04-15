abb35b6279d2dce4076008ebc67480ff0943e984
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-04-15 09:42:36 +0600

    Update Add Product

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import DashboardTitle from '../../Components/DashboardTitle/DashboardTitle';
 import { Input, Option, Select, Textarea, Button } from '@material-tailwind/react';
+import axios from 'axios';
+import { Toaster, toast } from 'react-hot-toast';
 
 const AddProduct = () => {
-    const handleAddProduct = (e) =>{
+    const url = 'http://localhost:5000/product'
+    const headers = {
+        'Content-Type': 'application/json',
+        "authorization": `Bearer ${localStorage.getItem('accessToken')}`
+    };
+    const handleAddProduct = (e) => {
         e.preventDefault()
         const title = e.target.title.value
         const description = e.target.description.value
@@ -12,6 +19,29 @@ const AddProduct = () => {
         const price = e.target.price.value
         const rating = e.target.price.value
         const count = e.target.price.value
+
+        const uploadProduct = {
+            title,
+            price,
+            description,
+            category,
+            image,
+            rating: {
+                rating,
+                count
+            }
+        }
+
+        axios.post(url, uploadProduct, { headers })
+            .then((response) => {
+                toast.success('Successfully Uploaded!')
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        e.target.reset();
+
     }
     return (
         <div>
@@ -19,35 +49,36 @@ const AddProduct = () => {
             <div>
                 <form onSubmit={handleAddProduct}>
                     <div className='my-3'>
-                        <Input label="Title" type='text' name='title' required/>
+                        <Input label="Title" type='text' name='title' required />
                     </div>
                     <div className='my-3'>
-                        <Textarea label="Description"  name='description' required/>
+                        <Textarea label="Description" name='description' required />
                     </div>
                     <div className='my-3'>
-                        <Input label="Image" type='text' name='image' required/>
+                        <Input label="Image" type='text' name='image' required />
                     </div>
                     <div className='my-5'>
                         <Select label="Select Version" name='category' required>
                             <Option className='capitalize'>men's clothing</Option>
-                            <Option className='capitalize'>jewelery</Option>
+                            <Option className='capitalize'>jewelry</Option>
                             <Option className='capitalize'>electronics</Option>
                             <Option className='capitalize'>women's clothing	</Option>
                             <Option>Material Tailwind Svelte</Option>
                         </Select>
                     </div>
                     <div className='my-3'>
-                        <Input label="Price" type='number' name='price' required/>
+                        <Input label="Price" type='number' name='price' required />
                     </div>
                     <div className='my-3'>
-                        <Input label="Rating" type='number' name='rating' required/>
+                        <Input label="Rating" type='number' name='rating' required />
                     </div>
                     <div className='my-3'>
-                        <Input label="Count" type='number' name='count' required/>
+                        <Input label="Count" type='number' name='count' required />
                     </div>
-                    <Button>Publish</Button>
+                    <Button type='submit'>Publish</Button>
                 </form>
             </div>
+            <Toaster />
         </div>
     );
 };
